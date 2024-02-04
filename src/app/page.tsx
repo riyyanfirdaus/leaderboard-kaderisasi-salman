338f264d1e530f72e92d53ce0670b95bbe60393b
riyyanfir <m.riyyan.firdaus@gmail.com>
2024-02-05 05:05:37 +0700

    feat: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,36 @@
+"use client";
 import { Navabar, Footer } from "@/components/layout";
-import { LeftOutlined } from "@ant-design/icons";
-import { Button } from "antd";
-
+import { LeaderboardData, TableColums } from "@/constants";
+import { SendOutlined } from "@ant-design/icons";
+import { Button, Flex, Form, Input, Table } from "antd";
 
 export default function Home() {
   return (
     <div className="w-full min-h-screen bg-[#F3F9FE]">
       <Navabar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Button className="bg-primary" icon={<LeftOutlined />} />
+      <main>
+        <div className="w-full h-screen pt-16 md:pt-32">
+          <div className="w-full px-4 max-w-6xl mx-auto md:px-6">
+            <div className="text-center">
+              <h1 className="font-bold text-2xl text-[#1D566D] mb-2">Leaderboard Prestasi Aktivis Salman</h1>
+              <p className="text-[#1D566D]">Dikelola oleh Bidang Mahasiswa, Kaderisasi, dan Alumni Masjid Salman ITB</p>
+            </div>
+
+            <Form>
+              <Flex gap="middle">
+                <Form.Item>
+                  <Input />
+                </Form.Item>
+
+                <Form.Item>
+                  <Button htmlType="submit" icon={<SendOutlined />} />
+                </Form.Item>
+              </Flex>
+            </Form>
+
+            <Table columns={TableColums} dataSource={LeaderboardData} />
+          </div>
+        </div>
       </main>
       <Footer />
     </div>
